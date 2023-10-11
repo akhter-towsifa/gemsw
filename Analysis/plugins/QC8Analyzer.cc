d352845f81d5b3d1a7822e7b2f33fe90f3cc2049
Towsifa Akhter <towsifa.akhter@gmail.com>
2023-10-11 14:42:17 +0200

    EDAnalyzer

--- Analysis/plugins/QC8Analyzer.cc
+++ Analysis/plugins/QC8Analyzer.cc
@@ -59,7 +59,7 @@ using namespace edm;
 struct QC8Data
 {
   void init();
-  TTree* book(TTree *t);//, int branch_type);
+  TTree* book(TTree *t);
   //==========Track Info=========
   float track_chi2;  
 };
@@ -70,16 +70,9 @@ void QC8Data::init()
   track_chi2 = 999999;
 }
 
-TTree* QC8Data::book(TTree *t){//, int branch_type){
+TTree* QC8Data::book(TTree *t){
   edm::Service< TFileService > fs;
-  //if (branch_type==1){
   t = fs->make<TTree>("analyzer", "analyzer");
-  //}
-  /*else if (branch_type==2){
-    t = fs->make<TTree>("plots", "plots");
-  }
-  else {std::cout << "branch number not one of the two listed ones" << std::endl;}
-  */
 
   //==========Track Info=========
   t->Branch("track_chi2", &track_chi2);
@@ -101,12 +94,16 @@ private:
 
   //member data
   edm::EDGetTokenT<reco::TrackCollection> tracks_;
+  //edm::Handle<vector<reco::TrackCollection>> tracks;
   edm::EDGetTokenT<vector<Trajectory>> trajs_;
   edm::EDGetTokenT<GEMRecHitCollection> gemRecHits_;
-  edm::ESGetToken<GEMGeometry, MuonGeometryRecord> GEMGeom_;
-  edm::ESGetToken<GEMGeomtery, MuonGeometryRecord> GEMGeomBeginRun_;
+  edm::ESHandle<GEMGeometry> GEMGeometry_;
+  const edm::ESGetToken<GEMGeometry, MuonGeometryRecord> GEMGeom_;
+  const edm::ESGetToken<GEMGeometry, MuonGeometryRecord> GEMGeomBeginRun_;
 
   edm::Service<TFileService> fs;
+  MuonServiceProxy* theService_;
+  //edm::ESHandle<vector<reco::TrackCollection>> tracks;
   
   bool debug;
 
@@ -120,14 +117,17 @@ QC8Analyzer::QC8Analyzer(const edm::ParameterSet& iConfig)
 {
   if (debug) {std::cout << "Begin QC8Analyzer" <<std::endl;}
   edm::ParameterSet serviceParameters = iConfig.getParameter<edm::ParameterSet>("ServiceParameters");
+  theService_ = new MuonServiceProxy(serviceParameters, consumesCollector());
 
-  tracks_ = consumes<reco::TrackCollection(iConfig.getParameter<edm::InputTag>("tracks"));
+  tracks_ = consumes<reco::Track(iConfig.getParameter<InputTag>("tracks"));
   trajs_ = consumes<vector<Trajectory>>(iConfig.getParameter<edm::InputTag>("trajs"));
   gemRecHits_ = consumes<GEMRecHitCollection>(iConfig.getParameter<edm::InputTag>("gemRecHitLabel"));
   debug = iConfig.getParameter<bool>("debug");
+
+  //tracks = iConfig.getParameter<edm::InputTag>("tracks");
 }
 
-void QC8TrackValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+void QC8Analyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
   edm::ESHandle<GEMGeometry> GEMGeom = iSetup.getHandle(GEMGeom_);
   const GEMGeometry* GEMGeometry_ = &*GEMGeom;
@@ -141,6 +141,8 @@ void QC8TrackValidation::analyze(const edm::Event& iEvent, const edm::EventSetup
   edm::Handle<GEMRecHitCollection> gemRecHits;
   iEvent.getByToken(gemRecHits_, gemRecHits);
 
+  TTree* tree;
+
   std::map<GEMDetId, TrajectoryStateOnSurface> tsos_Map;
   for (std::vector<reco::Track>::const_iterator track = tracks->begin(); track != tracks->end(); ++track){
     data_.init();
@@ -148,11 +150,30 @@ void QC8TrackValidation::analyze(const edm::Event& iEvent, const edm::EventSetup
     auto traj = trajs->begin();
     for (auto traj_measurements : traj->measurements()) {
       auto tsos = traj_measurements.predictedState();
-      auto rechit = traj_measurements.rechit();
+      auto rechit = traj_measurements.recHit();
       auto GEMID = GEMDetId(rechit->geographicalId());
       tsos_Map[GEMID] = tsos;
     }
 
+    for (auto hit : track->recHits()){
+      auto etaPart = GEMGeometry_->etaPartition(hit->geographicalId());
+      auto etaPartId = etaPart->id();
+
+      if (tsos_Map.find(etaPartId) == tsos_Map.end()) continue;
+      auto tsos = tsos_Map[etaPartId];
+
+      auto track_LP = tsos.localPosition();
+
+      auto ch = etaPartId.chamber();
+      auto lay = etaPartId.layer();
+      auto iEta = etaPartId.ieta();
+
+      auto strip = int(etaPart->strip(track_LP));
+      int module = (16 - iEta)/4 + 1 + (2 - lay)*4;
+      int sector = 1 - ((16 - iEta) % 4 / 2);
+      if (debug) std::cout << "GEM chamber:layer:iEta " << ch << ":" << lay << ":" << iEta << std::endl;
+      if (debug) std::cout << "strip:module:sector " << strip << ":" << module << ":" << sector <<std::endl;
+    }
     
     tree->Fill();
   }
